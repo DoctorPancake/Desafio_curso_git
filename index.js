052cdcf732bf66c8467edf31d83f9701334d2fd0
DoctorMuffin <eduardo.vn03@gmail.com>
2018-08-04 22:37:34 -0500

    añadiendo funciones

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("si charcha");
+var texto = document.getElementById('numeros');
+var boton = document.getElementById("boton");
+
+
+boton.addEventListener("click", fibonacci);
+
+function fibonacci()
+{
+  console.log(texto.value);
+}
